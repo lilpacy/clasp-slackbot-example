75d0b7ac5f164884b6b56503e80d8685a1f649d5
LilPacy <revivedtomorrow@gmail.com>
2019-11-16 01:50:47 +0900

    update scraping logic

--- lib/Code.ts
+++ lib/Code.ts
@@ -1,39 +1,103 @@
 var webHookUrl = process.env.WEB_HOOK_URL;
-var username = process.env.USER_NAME;  // 通知時に表示されるユーザー名
-var icon_url = process.env.ICON_URL;  // 通知時に表示されるアイコン
+var username = process.env.USER_NAME; // 通知時に表示されるユーザー名
+var icon_url = process.env.ICON_URL; // 通知時に表示されるアイコン
 var devChannel = process.env.DEV_CHANNEL;
 var prodChannel = process.env.PROD_CHANNEL;
 
-export function messagePush(){
-  var html = UrlFetchApp.fetch('https://newspicks.com/').getContentText();
-  var top = Parser.data(html).from('<div class="section top-news centering-page-content">').to('</span> </div> </div> </div>  </div> </div>').build();
-  var articles = Parser.data(top).from('<div class="news-card vertical" data-id="').to('</span> </div> </div> </div>').iterate();
-  var messages = 'Current popular news! \n';
-  articles.forEach(function(ele,i){
-    var link = Parser.data(ele).from('" href="/news/').to('?ref=index').build();
-    var title = Parser.data(ele).from('<div class="title _ellipsis">').to('</div>').build().replace(/&quot;/g,'"');
-    var pickNum = Parser.data(ele).from('<span class="value">').to('</span>').build();
-    messages += (i+1).toString(10) + ':<https://newspicks.com/news/' + link + '|' + title + '> ' + pickNum + ' picks' + '\n'
+export function messagePush() {
+  var html = UrlFetchApp.fetch("https://qiita.com/").getContentText();
+  var top = Parser.data(html)
+    .from(
+      'Milestones</option></select></div></div><div class="p-home_main mb-3 mr-0@s">'
+    )
+    .to(
+      '&quot;scope&quot;:&quot;daily&quot;}"></div></div><div class="p-home_aside">'
+    )
+    .build();
+  var articles = Parser.data(top)
+    .from(";:[],&quot;isLikedByViewer")
+    .to("&quot;}}},{&quot;followingLikers&quot")
+    .iterate();
+  articles.pop(); // 最後に余計な配列が1つ入るので切り落とす
+  var messages = "Today's popular articles!\n";
+  var i = 0;
+  var items = [];
+  articles.forEach(function(ele) {
+    var tmp = Parser.data(ele)
+      .from("esCount&quot;:")
+      .to(",&quot;title&quot;:&quot;")
+      .build();
+    var item = {};
+    item.likes = parseInt(tmp, 10);
+    if (true) {
+      item.uuid = Parser.data(ele)
+        .from("&quot;,&quot;uuid&quot;:&quot;")
+        .to(
+          "&quot;,&quot;author&quot;:{&quot;profileImageUrl&quot;:&quot;https"
+        )
+        .build();
+      item.title = Parser.data(ele)
+        .from(",&quot;title&quot;:&quot;")
+        .to("&quot;,&quot;uuid&quot;:&quot;")
+        .build()
+        .replace(/&quot;/g, '"');
+      item.author = Parser.data(ele)
+        .from("&quot;,&quot;urlName&quot;:&quot;")
+        .to("")
+        .build();
+      items.push(item);
+      // messages += (i + 1).toString(10) + ':<https://qiita.com/' + author + '/items/' + uuid + '|' + title + '> ' + JSON.stringify(likes) + ' likes' + '\n';
+      i++;
+    } else {
+    }
   });
-  var jsonData =
-      {
-        "username" : username,
-        "icon_url": icon_url,
-        "text" : messages,
-        "channel" : devChannel
-      };
-  var payload = JSON.stringify(jsonData);
 
-  var options =
-      {
-        "method" : "post",
-        "contentType" : "application/json",
-        "payload" : payload
-      };
+  items.sort(function(a, b) {
+    return b.likes - a.likes;
+  }); // descending order
+  items.map(function(item, i) {
+    var line =
+      i == 0 || i == 10 || i == 20
+        ? "\n　▼ Rank " +
+          JSON.stringify(i + 1) +
+          "~" +
+          JSON.stringify(i + 10) +
+          " articles" +
+          "\n\n"
+        : "";
+    messages +=
+      line +
+      "　　" +
+      (i + 1).toString(10) +
+      ":<https://qiita.com/" +
+      "kokodo-demoiinyo" +
+      "/items/" +
+      item.uuid +
+      "|" +
+      item.title +
+      "> " +
+      JSON.stringify(item.likes) +
+      " likes" +
+      "\n";
+  });
+
+  var jsonData = {
+    username: username,
+    icon_url: icon_url,
+    text: messages,
+    channel: devChannel
+  };
+  var payload = JSON.stringify(jsonData);
+  var options = {
+    method: "post",
+    contentType: "application/json",
+    payload: payload
+  };
+
   try {
-      UrlFetchApp.fetch(webHookUrl, options);
+    Logger.log(UrlFetchApp.fetch(webHookUrl, options));
   } catch (e) {
-      Logger.log(e);
+    Logger.log(e);
   }
 }
 
